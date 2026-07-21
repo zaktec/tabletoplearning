d56b42d758e667642e604f72941466109b392156
Sheraz <csheraz@hotmail.com>
2026-07-21 22:24:34 +0100

    Fix GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zaktec.github.io',
+  base: '/tabletoplearning',
+});
